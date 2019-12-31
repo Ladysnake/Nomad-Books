d69d40ea0f33214c3ba06cbbe70de835b4c866d9
doctor4t <doctor4t@gmail.com>
2019-12-31 19:19:31 +0100

    Add grass page to various chest loot tables

--- src/main/java/ladysnake/nomadbooks/NomadBooks.java
+++ src/main/java/ladysnake/nomadbooks/NomadBooks.java
@@ -1,14 +1,77 @@
 package ladysnake.nomadbooks;
 
+import ladysnake.nomadbooks.common.item.GrassPageItem;
 import ladysnake.nomadbooks.common.item.NomadBookItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class NomadBooks implements ModInitializer {
+    public static final String MODID = "nomadbooks";
+
+    private static final Identifier DUNGEON_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/simple_dungeon");
+    private static final Identifier MINESHAFT_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/abandoned_mineshaft");
+    private static final Identifier TEMPLE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/jungle_temple");
+    private static final Identifier PYRAMID_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/desert_pyramid");
+    private static final Identifier STRONGHOLD_LIBRARY_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/stronghold_library");
+
+    public static Item NOMAD_BOOK;
+    public static Item GRASS_PAGE;
+
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, "nomadbooks:nomad_book", new NomadBookItem((new Item.Settings()).maxCount(1).group(ItemGroup.MISC)));
+        GRASS_PAGE = registerItem(new GrassPageItem((new Item.Settings()).group(ItemGroup.MISC).rarity(Rarity.UNCOMMON)), "grass_page");
+        NOMAD_BOOK = registerItem(new NomadBookItem((new Item.Settings()).maxCount(1).group(ItemGroup.MISC).rarity(Rarity.RARE)), "nomad_book");
+
+        // add grass pages to dungeons, mineshafts, jungle temples, desert pyramids and stronghold libraries chests loot tables
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (DUNGEON_CHEST_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .withRolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(GRASS_PAGE));
+
+                supplier.withPool(poolBuilder);
+            }
+            if (MINESHAFT_CHEST_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .withRolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(GRASS_PAGE));
+
+                supplier.withPool(poolBuilder);
+            }
+            if (TEMPLE_CHEST_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .withRolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(GRASS_PAGE));
+
+                supplier.withPool(poolBuilder);
+            }
+            if (PYRAMID_CHEST_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .withRolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(GRASS_PAGE));
+
+                supplier.withPool(poolBuilder);
+            }
+            if (STRONGHOLD_LIBRARY_CHEST_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .withRolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(GRASS_PAGE));
+
+                supplier.withPool(poolBuilder);
+            }
+        });
+    }
+
+    public static Item registerItem(Item item, String name) {
+        Registry.register(Registry.ITEM, "nomadbooks:" + name, item);
+        return item;
     }
 }
